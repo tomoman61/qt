2fe304329dd4d57f7e7ace17dac3fd31db744e26
tomoman61 <inotomo5@gmail.com>
2024-05-02 01:44:38 +0900

    fmt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ const copyToClipboard = async (
 	matter: string,
 	hypothesis: string,
 	survey: string,
-  memo: string,
+	memo: string,
 ) => {
 	try {
-		navigator.clipboard.writeText(`#${text}\n\n#事象\n${matter}\n\n#仮説\n${hypothesis}\n\n#調査したこと\n${survey}\n\n#備考\n${memo}`);
+		navigator.clipboard.writeText(
+			`#${text}\n\n#事象\n${matter}\n\n#仮説\n${hypothesis}\n\n#調査したこと\n${survey}\n\n#備考\n${memo}`,
+		);
 	} catch (err) {
 		console.error(err);
 	}
@@ -22,9 +24,9 @@ export default function Home() {
 	const [matter, setMatter] = useState<string>("");
 	// 仮説
 	const [hypothesis, setHypothesis] = useState<string>("");
-  // 調査したこと
+	// 調査したこと
 	const [survey, setSurvey] = useState<string>("");
-  // 備考
+	// 備考
 	const [memo, setMemo] = useState<string>("");
 
 	// クリア
@@ -32,8 +34,8 @@ export default function Home() {
 		setType("");
 		setMatter("");
 		setHypothesis("");
-    setSurvey("");
-    setMemo("");
+		setSurvey("");
+		setMemo("");
 	};
 
 	return (
@@ -85,7 +87,7 @@ export default function Home() {
 					className="textarea textarea-bordered resize-none"
 					name={matter}
 					value={matter}
-          id="matter"
+					id="matter"
 					onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
 						setMatter(e.target.value)
 					}
@@ -99,38 +101,38 @@ export default function Home() {
 				<textarea
 					className="textarea textarea-bordered resize-none"
 					name={hypothesis}
-          value={hypothesis}
-          id="hypothesis"
+					value={hypothesis}
+					id="hypothesis"
 					onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
 						setHypothesis(e.target.value)
 					}
 				/>
 			</label>
 
-      <label className="form-control max-w-xs mb-3">
+			<label className="form-control max-w-xs mb-3">
 				<div className="label">
 					<span className="label-text">#調査したこと</span>
 				</div>
 				<textarea
 					className="textarea textarea-bordered resize-none"
 					name={survey}
-          value={survey}
-          id="survey"
+					value={survey}
+					id="survey"
 					onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
 						setSurvey(e.target.value)
 					}
 				/>
 			</label>
 
-      <label className="form-control max-w-xs mb-3">
+			<label className="form-control max-w-xs mb-3">
 				<div className="label">
 					<span className="label-text">#備考</span>
 				</div>
 				<textarea
 					className="textarea textarea-bordered resize-none"
 					name={memo}
-          value={memo}
-          id="memo"
+					value={memo}
+					id="memo"
 					onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
 						setMemo(e.target.value)
 					}
@@ -155,22 +157,20 @@ function IconContentCopy(props: React.SVGProps<SVGSVGElement>) {
 	);
 }
 
-
 function IconClear(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
-    <svg
-      viewBox="0 0 1024 1024"
-      fill="currentColor"
-      height="1.7em"
-      width="1.7em"
-      {...props}
-    >
-      <defs>
-        <style />
-      </defs>
-      <path d="M899.1 869.6l-53-305.6H864c14.4 0 26-11.6 26-26V346c0-14.4-11.6-26-26-26H618V138c0-14.4-11.6-26-26-26H432c-14.4 0-26 11.6-26 26v182H160c-14.4 0-26 11.6-26 26v192c0 14.4 11.6 26 26 26h17.9l-53 305.6c-.3 1.5-.4 3-.4 4.4 0 14.4 11.6 26 26 26h723c1.5 0 3-.1 4.4-.4 14.2-2.4 23.7-15.9 21.2-30zM204 390h272V182h72v208h272v104H204V390zm468 440V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H416V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H202.8l45.1-260H776l45.1 260H672z" />
-    </svg>
-  );
+	return (
+		// biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+		<svg
+			viewBox="0 0 1024 1024"
+			fill="currentColor"
+			height="1.7em"
+			width="1.7em"
+			{...props}
+		>
+			<defs>
+				<style />
+			</defs>
+			<path d="M899.1 869.6l-53-305.6H864c14.4 0 26-11.6 26-26V346c0-14.4-11.6-26-26-26H618V138c0-14.4-11.6-26-26-26H432c-14.4 0-26 11.6-26 26v182H160c-14.4 0-26 11.6-26 26v192c0 14.4 11.6 26 26 26h17.9l-53 305.6c-.3 1.5-.4 3-.4 4.4 0 14.4 11.6 26 26 26h723c1.5 0 3-.1 4.4-.4 14.2-2.4 23.7-15.9 21.2-30zM204 390h272V182h72v208h272v104H204V390zm468 440V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H416V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H202.8l45.1-260H776l45.1 260H672z" />
+		</svg>
+	);
 }
-
